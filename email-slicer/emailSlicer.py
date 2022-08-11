5da581de4a9d4cae0ecf3975379b82c7175f1fcb
shahlaibadzadeh <shahlaibadzadeh@gmail.com>
2022-08-11 16:50:02 +0400

    Multiple inputs can be entered

--- email-slicer/emailSlicer.py
+++ email-slicer/emailSlicer.py
@@ -1,3 +1,4 @@
-email = str(input())
-name = email.split("@")[0]
-print(f'Hey, {name.capitalize()}. Welcome on board!')
+emails = input().split()
+for email in emails:
+    name = email.split("@")[0]
+    print(f'Hey, {name.capitalize()}. Welcome on board!')
