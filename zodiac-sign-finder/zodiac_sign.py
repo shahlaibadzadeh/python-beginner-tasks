d951f90f2d7c21666e8c89bf7d4e4e8f38343b1b
shahlaibadzadeh <shahlaibadzadeh@gmail.com>
2022-08-21 17:36:13 +0400

    Date with datetime module

--- zodiac-sign-finder/zodiac_sign.py
+++ zodiac-sign-finder/zodiac_sign.py
@@ -1,63 +1,65 @@
-date = input("Enter date: ")
-day = int(date.split('.')[0])
-mon = date.split('.')[1]
+from datetime import datetime
+user_birth = input("Enter birthday: (17.02.2000) ")
+birth_dateform = datetime.strptime(user_birth, "%d.%m.%Y")
+mon = birth_dateform.month
+day = birth_dateform.day
 
-if (mon == '01'):
+if (mon == 1):
     if (day < 20):
         sign = "Capricornus"  
     else: sign = "Aquarius"
 
-elif (mon == '02'):
+elif (mon == 2):
     if (day < 19):
         sign = "Aquarius"  
     else: sign = "Pisces"
 
-elif (mon == '03'):
+elif (mon == 3):
     if (day < 21):
         sign = "Pisces"
     else: sign = "Aries"
 
-elif (mon == '04'):
+elif (mon == 4):
     if (day < 20):
         sign = "Aries"
     else: sign = "Taurus"
 
-elif (mon == '05'):
+elif (mon == 5):
     if (day < 21):
         sign = "Taurus"
     else: sign = "Gemini"
 
-elif (mon == '06'):
+elif (mon == 6):
     if (day < 22):
         sign = "Gemini"
     else: sign = "Cancer"
 
-elif (mon == '07'):
+elif (mon == 7):
     if (day < 23):
         sign = "Cancer"
     else: sign = "Leo"
 
-elif (mon == '08'):
+elif (mon == 8):
     if (day < 23):
         sign = "Leo"
     else: sign = "Virgo"
 
-elif (mon == '09'):
+elif (mon == 9):
     if (day < 23):
         sign = "Virgo"
     else: sign = "Libra"
 
-elif (mon == '10'):
+elif (mon == 10):
     if (day < 24):
         sign = "Libra"
     else: sign = "Scorpius"
 
-elif (mon == '11'):
+elif (mon == 11):
     if (day < 22):
         sign = "Scorpius"
     else: sign = "Sagittarius"
 
-elif (mon == '12'):
+elif (mon == 12):
     if (day < 22):
         sign = "Sagittarius"
     else: sign = "Capricornus"
